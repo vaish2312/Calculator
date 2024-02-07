5db3265da9f3e516a3ae05637a2cfdfc1abe5949
vaishnavi santosh aher <102506754+vaish2312@users.noreply.github.com>
2024-02-07 10:44:41 +0530

    Calculator.java

--- calculator/Calculator.java
+++ calculator/Calculator.java
@@ -4,18 +4,25 @@ import java.util.ArrayList;
 
 public class Calculator extends basiccal {
 
-	public static void main(String[] args) {
-		basiccal cal= new Calculator();
-		
-		 ArrayList<Double> numbers = cal.getInputNumbers();
-	        System.out.println("Result: " + cal.performOperation());
+	 @Override
+	    public void displayMenu() {
+	        System.out.println("\nMenu:");
+	        System.out.println("1. Addition");
+	        System.out.println("2. Subtraction");
+	        System.out.println("3. Multiplication");
+	        System.out.println("4. Division");
+	        System.out.println("5. Square");
+	        System.out.println("6. Square Root");
 	    }
 
-	@Override
-	public void closeScanner() {
-		// TODO Auto-generated method stub
-		
-	}
-	}
+	    @Override
+	    public void closeScanner() {
+	        scanner.close();
+	    }
 
-
+	    public static void main(String[] args) {
+	        CalculatorOperation calc = new Calculator();
+	        calc.performOperation();
+	        calc.closeScanner();
+	    }
+	}
